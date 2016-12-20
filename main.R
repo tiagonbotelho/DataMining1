d1f08784d33073c71865121d49e28ac69aa7f034
Tiago Botelho <tiagonbotelho@hotmail.com>
2016-12-20 16:33:55 +0000

    makes breakin changhe and solves the cure for cancer

--- main.R
+++ main.R
@@ -1,5 +1,10 @@
 library(gdata)
+library(ggplot2)
 data_path <- "./crime.xls"
 info <- read.xls(data_path, sheet=1)
-info
 
+split <- strsplit(as.character(info$BlockRange), "-")
+info$BlockRange <- order(sapply(split, "[", 1))
+
+ola <- info[startsWith(info$Beat, "10"),]
+ggplot(ola, aes(x=ola$Beat, y=ola$BlockRange, color=ola$Type)) + geom_point()
